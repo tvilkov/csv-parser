5a0aeb487b5cdc4574240cdce15d6dda9f89b3d6
Mikhail Tripolitov <m.tripolitov@unistream.com>
2014-02-12 10:40:54 +0400

    Introduce VersionInfo.cs

---
+++ src/VersionInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0.0")]
+[assembly: AssemblyDescription("Local developement build.")]
